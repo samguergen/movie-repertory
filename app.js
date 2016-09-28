f85855a21758e9016af013cfecc7c0b2af2b8e8c
Samantha Guergenenov <samguergen@gmail.com>
2016-09-27 21:36:22 -0400

    basic json displaying'

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 angular
-    .module('myApp', ['$http'])
-    .controller('MainCtrl', ['$scope', '$location', '$window', function($scope, $location, $window){
+    .module('movieApp', [])
+    .service('SortService', ['$log', function ($log) {
 
+        return {
+            sort: function() {
+
+            }
+        };
+
+    }])
+    .controller('MovieCtrl', ['$scope', '$location', '$http', '$window', 'SortService', function($scope, $location, $http, $window, SortService){
       console.log('init main ng controller');
+
+      $http.get('movies.json').then(function(data) {
+
+        $scope.movies = data.data
+      }, function(error) {
+        console.log("The movie list could not be loaded. Error Details: ", error);
+      });
+
+
+      $scope.sortList = SortService;
+      // $scope.sortList = SortService.sort;
 }])
