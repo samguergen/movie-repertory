f5bcc0fbba914a69672380e69bd20448f2c25913
Samantha Guergenenov <samguergen@gmail.com>
2016-09-27 22:22:07 -0400

    add to lst

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ angular
         console.log("The movie list could not be loaded. Error Details: ", error);
       });
 
-
       $scope.sortList = SortService;
       // $scope.sortList = SortService.sort;
+
+      $scope.addToList = function(){
+        $scope.movies.push($scope.newMovie);
+        $scope.newMovie = {};
+        console.log('movies are ', $scope.movies);
+      }
+
 }])
